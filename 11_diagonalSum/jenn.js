8f82558d9a67596282c40b363382638a0eb636b3
Jennifer Bennett <jennbbennett@hotmail.com>
2015-12-10 13:37:09 -0700

    solution ro diagonal sum

---
+++ 11_diagonalSum/jenn.js
@@ -0,0 +1,20 @@
+function diagonalSum(array){
+    var sum = 0;
+    for(i = 0; i < array.length; i++){
+      for(j = 0; j < array[i].length; j++){
+      if(i === j){
+        sum += array[j][i];
+      }
+    }
+  }
+    return sum;
+}
+
+console.log(diagonalSum([[1, 0, 0, 0],
+
+[0, 1, 0, 0],
+
+[0, 0, 1, 0],
+
+[0, 0, 0, 1]]
+));
